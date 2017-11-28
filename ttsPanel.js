be25a4627b2cd921f1b1fa13169117cc02ca52d0
ettoolong <ettoolong@hotmail.com>
2017-11-28 23:09:58 +0800

    1. Fix preference page bug.

--- ttsPanel.js
+++ ttsPanel.js
@@ -482,7 +482,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
   // Fix for Fx57 bug where bundled page loaded using
   // browser.windows.create won't show contents unless resized.
   // See https://bugzilla.mozilla.org/show_bug.cgi?id=1402110
-  browser.windows.getCurrent((win) => {
+  if(!document.getElementById('ttsfox-option')) {
+    browser.windows.getCurrent((win) => {
       browser.windows.update(win.id, {width:win.width+1})
-  })
+    });
+  }
 });
